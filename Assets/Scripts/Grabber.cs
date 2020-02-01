f272f0abd80c2038e476324440564076de2fd661
Aleksi Toivonen <aleksi.toivonen@zaibatsu.fi>
2020-02-01 17:27:13 +0200

    grabbing controls

--- Assets/Scripts/Grabber.cs
+++ Assets/Scripts/Grabber.cs
@@ -5,23 +5,45 @@ using UnityEngine;
 public class Grabber : MonoBehaviour
 {
     public LayerMask GrabbableLayer;
+    public float DragForce = 100.0f;
 
     private bool holding = false;
-    private readonly float maxDistance = 2.0f;
+    private readonly float maxDistance = 3.0f;
+    private readonly float holdDistance = 2.2f;
     private RaycastHit hit;
     private Rigidbody holdingRb;
-    private float DragForce = 10.0f;
+
+    [SerializeField]
+    private AnimationCurve dragCurve;
 
     // Update is called once per frame
     void Update()
     {
         if (holding)
         {
-            Vector3 targetPos = Camera.main.transform.position + Camera.main.transform.forward * maxDistance;
-            holdingRb.AddForce(targetPos - holdingRb.transform.position * Time.deltaTime * DragForce);
+            Debug.Log("move obj");
+            Vector3 targetPos = Camera.main.transform.position + Camera.main.transform.forward * holdDistance;
+            float distance = Vector3.Distance(holdingRb.transform.position, targetPos);
+            Vector3 dir = targetPos - holdingRb.position;
+            holdingRb.velocity = dir * dragCurve.Evaluate(distance);
+            holdingRb.transform.forward = Camera.main.transform.forward;
+
+            //holdingRb.transform.position = Vector3.Lerp(holdingRb.transform.position, targetPos, 0.5f);
+            /*if (distance > 2)
+            {
+                holdingRb.transform.position = Vector3.Lerp(holdingRb.transform.position, targetPos, 0.9f);
+            }
+            else
+            {
+                holdingRb.velocity = (targetPos - holdingRb.transform.position) * DragForce;
+                //holdingRb.AddForce(targetPos - holdingRb.transform.position * Time.deltaTime * DragForce);
+            }*/
+
             if (Input.GetKeyDown(KeyCode.E))
             {
+                Debug.Log("let go");
                 holding = false;
+                holdingRb.useGravity = true;
                 holdingRb = null;
             }
         }
@@ -33,6 +55,8 @@ public class Grabber : MonoBehaviour
                 Debug.Log("raycast hit");
                 holdingRb = hit.collider.GetComponent<Rigidbody>();
                 holdingRb.isKinematic = false;
+                holdingRb.transform.SetParent(null);
+                holdingRb.useGravity = false;
                 holding = true;
             }
         }
