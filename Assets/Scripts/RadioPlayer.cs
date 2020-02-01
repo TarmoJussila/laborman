cf178d8817cfeb8fada1a56443eb7790639057a8
Tarmo Jussila <tarmo.jussila@zaibatsu.fi>
2020-02-01 17:50:44 +0200

    Audio controller and radio player. No sounds yet.

---
+++ Assets/Scripts/RadioPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioPlayer : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource.clip = GetRandomAudioClip();
+        audioSource.Play();
+    }
+
+    private void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.clip = GetRandomAudioClip();
+            audioSource.Play();
+        }
+    }
+
+    private AudioClip GetRandomAudioClip()
+    {
+        return AudioController.Instance.AudioClips[Random.Range(0, AudioController.Instance.AudioClips.Count)];
+    }
+}
