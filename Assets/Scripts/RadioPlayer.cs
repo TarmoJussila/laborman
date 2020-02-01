61696088c5e6c0dd7501d041ab2f989aba6ea682
Tarmo Jussila <tarmo.jussila@zaibatsu.fi>
2020-02-01 20:59:47 +0200

    Radio is smaller.

--- Assets/Scripts/RadioPlayer.cs
+++ Assets/Scripts/RadioPlayer.cs
@@ -8,6 +8,7 @@ public class RadioPlayer : MonoBehaviour
 
     private void Start()
     {
+        audioSource.loop = false;
         audioSource.clip = GetRandomAudioClip();
         audioSource.Play();
     }
