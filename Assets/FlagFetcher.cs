a198deab03b6412a686f7ee8e9a5d33d5bdfd559
Tarmo Jussila <tarmo.jussila@zaibatsu.fi>
2020-02-01 04:53:01 +0200

    Van has locale-dependent flags on it.

---
+++ Assets/FlagFetcher.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagFetcher : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer flagRenderer;
+
+    private void Start()
+    {
+        flagRenderer.sprite = null;
+
+        StartCoroutine(FetchFlagSprite());
+    }
+
+    private IEnumerator FetchFlagSprite()
+    {
+        while (flagRenderer.sprite == null)
+        {
+            yield return new WaitForSeconds(0.5f);
+
+            var flagSprite = LocaleController.Instance.CurrentFlagSprite;
+
+            if (flagSprite != null)
+            {
+                flagRenderer.sprite = flagSprite;
+            }
+        }
+    }
+}
