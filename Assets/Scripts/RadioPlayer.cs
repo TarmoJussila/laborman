c581ee46e3c2dd2c2f7232c4c9cb2446e91c9c36
Tarmo Jussila <tarmo.jussila@zaibatsu.fi>
2020-02-02 02:29:30 +0200

    Update RadioPlayer.cs

--- Assets/Scripts/RadioPlayer.cs
+++ Assets/Scripts/RadioPlayer.cs
@@ -6,6 +6,9 @@ public class RadioPlayer : MonoBehaviour
 {
     [SerializeField] private AudioSource audioSource;
 
+    private bool didPlayRadioStaticSound = false;
+    private int lastMusicClipIndex = -1;
+
     private void Start()
     {
         audioSource.loop = false;
@@ -17,13 +20,36 @@ public class RadioPlayer : MonoBehaviour
     {
         if (!audioSource.isPlaying)
         {
-            audioSource.clip = GetRandomMusicClip();
-            audioSource.Play();
+            if (!didPlayRadioStaticSound)
+            {
+                audioSource.clip = GetRandomRadioStaticClip();
+                audioSource.Play();
+                didPlayRadioStaticSound = true;
+            }
+            else
+            {
+                audioSource.clip = GetRandomMusicClip();
+                audioSource.Play();
+                didPlayRadioStaticSound = false;
+            }
         }
     }
 
     private AudioClip GetRandomMusicClip()
     {
-        return AudioController.Instance.MusicClips[Random.Range(0, AudioController.Instance.MusicClips.Count)];
+        int newMusicClipIndex = Random.Range(0, AudioController.Instance.MusicClips.Count);
+
+        while (newMusicClipIndex == lastMusicClipIndex && AudioController.Instance.MusicClips.Count > 1)
+        {
+            newMusicClipIndex = Random.Range(0, AudioController.Instance.MusicClips.Count);
+        }
+
+        lastMusicClipIndex = newMusicClipIndex;
+        return AudioController.Instance.MusicClips[newMusicClipIndex];
+    }
+
+    private AudioClip GetRandomRadioStaticClip()
+    {
+        return AudioController.Instance.RadioStaticClips[Random.Range(0, AudioController.Instance.RadioStaticClips.Count)];
     }
 }
