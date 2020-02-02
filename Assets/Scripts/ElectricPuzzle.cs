74d50451c823983a904f8a53b1776c7acbc6023d
Tarmo Jussila <tarmo.jussila@zaibatsu.fi>
2020-02-02 13:55:59 +0200

    Electrix puzzle works.

--- Assets/Scripts/ElectricPuzzle.cs
+++ Assets/Scripts/ElectricPuzzle.cs
@@ -7,11 +7,29 @@ public class Fuse
 {
     public bool On = false;
     public MeshRenderer MeshRenderer;
+    public int FuseIndex = -1;
+    public int PairedFuseIndex = -2;
 
-    public void Toggle(Material onMaterial, Material offMaterial)
+    private Fuse pairedFuse = null;
+
+    public void Initialize(Fuse pairedFuse, int fuseIndex, int pairedFuseIndex)
+    {
+        this.pairedFuse = pairedFuse;
+        FuseIndex = fuseIndex;
+        PairedFuseIndex = pairedFuseIndex;
+    }
+
+    public void Toggle(Material onMaterial, Material offMaterial, float onPositionY, float offPositionY, bool doTogglePairedFuse = true)
     {
         On = !On;
         MeshRenderer.material = On ? onMaterial : offMaterial;
+        MeshRenderer.transform.localPosition = new Vector3(MeshRenderer.transform.localPosition.x, On ? onPositionY : offPositionY,
+            MeshRenderer.transform.localPosition.z);
+
+        if (doTogglePairedFuse && FuseIndex != PairedFuseIndex && PairedFuseIndex >= 0 && pairedFuse != null)
+        {
+            pairedFuse.Toggle(onMaterial, offMaterial, onPositionY, offPositionY, false);
+        }
     }
 }
 
@@ -20,6 +38,8 @@ public class ElectricPuzzle : Puzzle
     [SerializeField] private List<Fuse> fuses = new List<Fuse>();
     [SerializeField] private Material onMaterial;
     [SerializeField] private Material offMaterial;
+    [SerializeField] private float onPositionY;
+    [SerializeField] private float offPositionY;
 
     private void Start()
     {
@@ -30,9 +50,20 @@ public class ElectricPuzzle : Puzzle
     {
         for (int i = 0; i < 5; i++)
         {
+            var randomFuseIndex = Random.Range(0, 5);
+            fuses[i].Initialize(fuses[randomFuseIndex], i, randomFuseIndex);
+
+            // NOTE: Make sure at least one fuse is not paired.
+            int nonPairedFuseIndex = Random.Range(0, 5);
+            fuses[nonPairedFuseIndex].Initialize(null, nonPairedFuseIndex, nonPairedFuseIndex);
+        }
+
+        // Scramble fuse box at start.
+        for (int i = 0; i < 5; i++)
+        {
             for (int j = 0; j < Random.Range(1, 3); j++)
             {
-                fuses[i].Toggle(onMaterial, offMaterial);
+                fuses[i].Toggle(onMaterial, offMaterial, onPositionY, offPositionY);
             }
         }
     }
@@ -43,7 +74,8 @@ public class ElectricPuzzle : Puzzle
         {
             if (fuseName.EndsWith((i + 1).ToString()))
             {
-                fuses[i].Toggle(onMaterial, offMaterial);
+                fuses[i].Toggle(onMaterial, offMaterial, onPositionY, offPositionY);
+                break;
             }
         }
     }
