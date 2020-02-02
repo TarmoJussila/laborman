00bf907b24923149b7c084f86cd6ef2b47d9bc5c
Aleksi Toivonen <aleksi.toivonen@zaibatsu.fi>
2020-02-02 03:25:04 +0200

    room stuff

---
+++ Assets/PuzzleRoom.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleRoom : MonoBehaviour
+{
+    private void Start()
+    {
+        RenovationController.Instance.RegisterRoom(this);
+    }
+
+    public Puzzle SpawnPuzzle()
+    {
+        GameObject prefab = RenovationController.Instance.RoomPuzzlePrefabs[Random.Range(0, RenovationController.Instance.RoomPuzzlePrefabs.Length)];
+        GameObject instance = Instantiate(prefab);        
+
+        instance.transform.position = transform.position;
+        instance.transform.forward = transform.forward;
+        Puzzle puzzle = instance.GetComponent<Puzzle>();        
+        return puzzle;
+    }
+
+    public void SpawnRoom()
+    {
+        GameObject prefab = RenovationController.Instance.RoomPrefabs[Random.Range(0, RenovationController.Instance.RoomPrefabs.Length)];
+        GameObject instance = Instantiate(prefab);
+
+        instance.transform.position = transform.position;
+        instance.transform.forward = transform.forward;
+    }
+
+}
